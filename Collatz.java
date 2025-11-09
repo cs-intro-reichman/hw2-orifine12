d64d6e01dbc2b20da8c0bd7ceee53d8c172d2027
Ori Fine <ori.fine@post.runi.ac.il>
2025-11-09 23:59:15 +0200

    collatz 1.1

--- Collatz.java
+++ Collatz.java
@@ -4,7 +4,7 @@ public class Collatz {
         int N = Integer.parseInt(args[0]); // highest seed
 		String mode = args [1];
 		
-
+	
         // For each seed from 1 to N:
         for (int i = 1; i <= N; i++) {
              int steps = 1;
@@ -14,7 +14,7 @@ public class Collatz {
             System.out.print(current); // Print the starting number
 			 }
 			// here ill put the while loop:
-            while (current != 1) {
+            do {
                 if (current % 2 == 0) {
                     current = current / 2;
                 } 
@@ -27,7 +27,7 @@ public class Collatz {
                 System.out.print(" " + current);
 				}
 				steps ++;
-                 }
+                 } while (current !=1);
 		 if (mode.equals("v"))
 		        {
 			System.out.print( " (" + steps + ")");
@@ -36,6 +36,7 @@ public class Collatz {
 			
         }
 		System.out.println("Every one of the first " + N +  " hailstone sequences reached 1.");
+ 
     }
 }
 // javac Collatz.java
