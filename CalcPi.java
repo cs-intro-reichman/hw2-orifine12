b08ac7e65ca8abce7181e4ddee39d770b1810ca9
Ori Fine <ori.fine@post.runi.ac.il>
2025-11-09 23:47:54 +0200

    collats

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,25 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int num = Integer.parseInt (args[0]);
+		double denominator = -1;
+		double ratio;
+		double pi = 0;
+
+		for (int i = 0; i < num; i++)
+		{
+			denominator = denominator + 2; 
+			ratio = 1 / denominator;
+			if (i%2==0)
+			{
+               pi = pi + ratio;
+			}
+			else 
+			{
+				pi = pi - ratio;
+			}
+		}
+        pi = pi*4;
+		System.out.println(pi);
 	}
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,42 @@
-// Demonstrates the Collatz conjecture.
+// Demonstrates the Collatz conjecture (no modes yet).
 public class Collatz {
-	public static void main(String args[]) {
-	    // Replace this comment with your code
-	}
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]); // highest seed
+		String mode = args [1];
+		
+
+        // For each seed from 1 to N:
+        for (int i = 1; i <= N; i++) {
+             int steps = 1;
+             int current = i;
+			 if (mode.equals("v"))
+			 {
+            System.out.print(current); // Print the starting number
+			 }
+			// here ill put the while loop:
+            while (current != 1) {
+                if (current % 2 == 0) {
+                    current = current / 2;
+                } 
+				else 
+				{
+                    current = current * 3 + 1;
+                }
+				if (mode.equals("v"))
+				{
+                System.out.print(" " + current);
+				}
+				steps ++;
+                 }
+		 if (mode.equals("v"))
+		        {
+			System.out.print( " (" + steps + ")");
+			System.out.println();
+				}
+			
+        }
+		System.out.println("Every one of the first " + N +  " hailstone sequences reached 1.");
+    }
 }
+// javac Collatz.java
+// java Collatz 8
